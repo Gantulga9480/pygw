9a5c6033a38c34387e94b446dcb6c9b0a71d49d7
Gantulga G <limited.tulgaa@gmail.com>
2022-11-25 22:59:53 +0800

    added pygame as core

--- Game/__init__.py
+++ Game/__init__.py
@@ -4,3 +4,4 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from .game import Game  # noqa
 from .scene import Scene, State  # noqa
 from .window import Window  # noqa
+import pygame as core  # noqa
